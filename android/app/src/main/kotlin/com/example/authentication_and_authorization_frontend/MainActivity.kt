d16ca35a52b6c4966c646874f8a553a8951c2568
Gabriel Araujo <gabriel.houth@gmail.com>
2022-08-13 15:26:45 -0300

    feat: initial commit

---
+++ android/app/src/main/kotlin/com/example/authentication_and_authorization_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.authentication_and_authorization_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:authentication_and_authorization_frontend/app/app.dart';
+
+void main() {
+  runApp(const App());
+}
