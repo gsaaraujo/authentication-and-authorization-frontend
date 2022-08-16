42dca6750b1e3c172c1291b094b94e7975732b10
Gabriel Araujo <gabriel.houth@gmail.com>
2022-08-16 09:33:22 -0300

    feat: add input and button components

---
+++ lib/src/app/components/input_text.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class InputText extends StatefulWidget {
+  const InputText({
+    Key? key,
+    required this.label,
+    required this.controller,
+    this.validator,
+    this.icon,
+    this.textInputType,
+    this.maxLength = 255,
+    this.isPassword = false,
+  }) : super(key: key);
+
+  final String label;
+  final int maxLength;
+  final bool isPassword;
+  final Widget? icon;
+  final TextInputType? textInputType;
+  final TextEditingController controller;
+  final Function(String)? validator;
+
+  @override
+  State<InputText> createState() => _TextFormFieldWidgetState();
+}
+
+class _TextFormFieldWidgetState extends State<InputText> {
+  bool hasText = false;
+  bool isPasswordHiden = false;
+
+  @override
+  void initState() {
+    isPasswordHiden = widget.isPassword;
+
+    widget.controller.addListener(() {
+      setState(() {
+        widget.controller.text.isEmpty ? hasText = false : hasText = true;
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      maxLength: widget.maxLength,
+      obscureText: isPasswordHiden,
+      controller: widget.controller,
+      keyboardType: widget.textInputType,
+      decoration: InputDecoration(
+        counterText: '',
+        labelText: widget.label,
+        prefixIcon: widget.icon,
+        suffixIcon: IconButton(
+          onPressed: () {
+            setState(() {
+              widget.isPassword
+                  ? isPasswordHiden = !isPasswordHiden
+                  : widget.controller.clear();
+            });
+          },
+          icon: Icon(
+            widget.isPassword
+                ? isPasswordHiden
+                    ? Icons.visibility_off_outlined
+                    : Icons.visibility_outlined
+                : hasText
+                    ? Icons.highlight_remove_rounded
+                    : null,
+          ),
+        ),
+      ),
+      validator: (String? value) {
+        if (value == null || value.trim().isEmpty) {
+          return 'This field is required';
+        }
+        if (widget.validator != null) return widget.validator!(value);
+        return null;
+      },
+    );
+  }
+}
---
+++ lib/src/app/components/loading_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:authentication_and_authorization_frontend/src/app/styles/app_texts.dart';
+import 'package:authentication_and_authorization_frontend/src/app/styles/app_colors.dart';
+
+class LoadingButton extends StatelessWidget {
+  const LoadingButton({
+    Key? key,
+    required this.title,
+    required this.onPress,
+    this.width,
+    this.isLoading = false,
+  }) : super(key: key);
+
+  final String title;
+  final VoidCallback onPress;
+  final double? width;
+  final bool isLoading;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: isLoading ? null : onPress,
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        minimumSize: Size(width ?? 0.0, 48.0),
+      ),
+      child: isLoading
+          ? const CircularProgressIndicator(color: AppColors.backgroundButton)
+          : Text(title, style: AppTexts.textButton),
+    );
+  }
+}
